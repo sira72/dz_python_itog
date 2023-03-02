3800d1d64f2fb0812ebb44538e7cc5e4e80f3534
Sira for git <sano72@mail.ru>
2023-03-02 12:31:12 +0300

    1ая задача

--- dz_zad_itog/zadacha_1.py
+++ dz_zad_itog/zadacha_1.py
@@ -9,3 +9,19 @@
 // --- '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430' - набор кодовых точек
 // --- используйте списки и циклы, не дублируйте функции
 // """
+
+FIRST_WORD = 'разработка'
+SECOND_WORD = 'сокет'
+THIRD_WORD = 'декоратор'
+word_lst = [FIRST_WORD, SECOND_WORD, THIRD_WORD]
+for element in word_lst:
+    print(element)
+    print(type(element))
+
+FIRST_UNIC = '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430'
+SECOND_UNIC = '\u0441\u043e\u043a\u0435\u0442'
+THIRD_UNIC = '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440'
+unic_lst = [FIRST_UNIC, SECOND_UNIC, THIRD_UNIC]
+for element in unic_lst:
+    print(element)
+    print(type(element))
